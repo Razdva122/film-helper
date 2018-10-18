39286ffead91bfaa74783c49fb7742e25f5e133e
Dmitrii <lexur-02@yandex.ru>
2018-10-18 13:34:18 +0500

    Added 3 films, fixed location of button

---
+++ Sources/js/films.js
@@ -0,0 +1,9 @@
+function Film(title,year,rating,info){
+	this.title=title;
+	this.year=year;
+	this.rating=rating;
+	this.info=info;
+}
+var shawshank=new Film("Побег из Шоушенка",1994,9.112,shawshankInfo);
+var greenMile=new Film("Зеленая миля",1999,9.062,greenMileInfo);
+var forrestGump=new Film("Форрест Гамп",1994,8.917,forrestGumpInfo);
