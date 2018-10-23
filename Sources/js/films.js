cb2583ba7685486f15ebdbca7711c566d0e5623c
Dmitrii <lexur-02@yandex.ru>
2018-10-23 21:06:55 +0500

    Fixed test, now is generate random film

--- Sources/js/films.js
+++ Sources/js/films.js
@@ -4,6 +4,9 @@ function Film(title,year,rating,info){
 	this.rating=rating;
 	this.info=info;
 }
+
 var shawshank=new Film("Побег из Шоушенка",1994,9.112,shawshankInfo);
 var greenMile=new Film("Зеленая миля",1999,9.062,greenMileInfo);
 var forrestGump=new Film("Форрест Гамп",1994,8.917,forrestGumpInfo);
+
+var arrayFilms=[shawshank,greenMile,forrestGump];
--- Sources/js/main.js
+++ Sources/js/main.js
@@ -1,7 +1,14 @@
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 $(document).ready(function(){
 	$(".header").css("border","3px solid red");
 });
 angular.module("myApp",[])
 .controller("firstCtrl",function ($scope) {
-	$scope.testing = "testing";
+	$scope.randomFilm = function(){
+		var randomNumber=getRandomInt(0,arrayFilms.length);
+		$scope.currentFilm=arrayFilms[randomNumber];
+	}
 });
