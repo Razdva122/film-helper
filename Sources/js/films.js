b481df5a3aee9d0f61b0e71b074a10c1fc830c58
Dmitrii <lexur-02@yandex.ru>
2018-10-28 19:57:21 +0500

    First code to pick film

--- Sources/js/films.js
+++ Sources/js/films.js
@@ -1,12 +1,21 @@
-function Film(title,year,rating,info){
+function Film(title,year,rating,info,dramaRating,comedyRating,
+	actionRating,fantasyRating,adventureRating){
 	this.title=title;
 	this.year=year;
 	this.rating=rating;
 	this.info=info;
+	this.dramaRating=dramaRating;
+	this.comedyRating=comedyRating;
+	this.actionRating=actionRating;
+	this.fantasyRating=fantasyRating;
+	this.adventureRating=adventureRating;
 }
 
-var shawshank=new Film("Побег из Шоушенка",1994,9.112,shawshankInfo);
-var greenMile=new Film("Зеленая миля",1999,9.062,greenMileInfo);
-var forrestGump=new Film("Форрест Гамп",1994,8.917,forrestGumpInfo);
+var shawshank=new Film("Побег из Шоушенка",1994,9.112,shawshankInfo,7,
+	4,2,0,4);
+var greenMile=new Film("Зеленая миля",1999,9.062,greenMileInfo,9,
+	0,3,0,2);
+var forrestGump=new Film("Форрест Гамп",1994,8.917,forrestGumpInfo,8,
+	2,2,0,6);
 
 var arrayFilms=[shawshank,greenMile,forrestGump];
--- Sources/js/main.js
+++ Sources/js/main.js
@@ -2,13 +2,44 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
+var filmsRanks=[];
+
 $(document).ready(function(){
 	//$(".header").css("border","3px solid red");
 });
 angular.module("myApp",[])
 .controller("firstCtrl",function ($scope) {
 	$scope.randomFilm = function(){
-		var randomNumber=getRandomInt(0,arrayFilms.length);
-		$scope.currentFilm=arrayFilms[randomNumber];
+		//var randomNumber=getRandomInt(0,arrayFilms.length);
+		//$scope.currentFilm=arrayFilms[randomNumber];
+		$scope.currentFilm=bestFilmForUser()[0];
 	}
 });
+
+function pickYourGenre(drama,comedy,action,fantasy,adventure){
+	for(var i=0;i<arrayFilms.length;i++){
+		var filmResult=[];
+		filmResult[0]=arrayFilms[i];
+		filmResult[1]=Math.abs(arrayFilms[i].dramaRating-drama);
+		filmResult[2]=Math.abs(arrayFilms[i].comedyRating-comedy);
+		filmResult[3]=Math.abs(arrayFilms[i].actionRating-action);
+		filmResult[4]=Math.abs(arrayFilms[i].fantasyRating-fantasy);
+		filmResult[5]=Math.abs(arrayFilms[i].adventureRating-adventure);
+		filmsRanks.push(filmResult);
+	}
+
+}
+
+function bestFilmForUser(){
+	var bestFilmPick=filmsRanks[0];
+	for(var i=1;i<arrayFilms.length;i++){
+		var resultOfCurrentBestFilm=bestFilmPick[1]+bestFilmPick[2]+
+			bestFilmPick[3]+bestFilmPick[4]+bestFilmPick[5];
+		var resultCurrentFilm=filmsRanks[i][1]+filmsRanks[i][2]+
+			filmsRanks[i][3]+filmsRanks[i][4]+filmsRanks[i][5];
+		if(resultCurrentFilm<resultOfCurrentBestFilm){
+			bestFilmPick=filmsRanks[i];
+		}		
+	}
+	return bestFilmPick;
+}
